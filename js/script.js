33e23c69863769929977239acdd747407dcbbe4d
Raffo98 <raf.riccardelli@gmail.com>
2022-01-13 00:28:12 +0100

    update index position elements

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-new fullpage('#fullpage', {
-	//options here
-	autoScrolling:true,
-	scrollHorizontally: true
-});
+// new fullpage('#single', {
+// 	//options here
+// 	autoScrolling:true,
+// 	scrollHorizontally: true
+// });
 
-//methods
-fullpage_api.setAllowScrolling(false);
+// //methods
+// fullpage_api.setAllowScrolling(false);
 
