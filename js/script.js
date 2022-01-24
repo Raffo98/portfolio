ea88cc5ffe2e7183b995c456f1ce59f6016e2571
Raffo98 <raf.riccardelli@gmail.com>
2022-01-24 23:33:45 +0100

    update single page

--- js/script.js
+++ js/script.js
@@ -4,11 +4,38 @@ document
   .querySelectorAll("#projects-wrapper ul li a")
   .forEach((element, index) => {
     element.addEventListener("mouseover", function () {
-      document.querySelector("#preview-image img").src = "assets/images/preview-images/" + `${index}` + ".png";
+      document.querySelector("#preview-image img").src =
+        "assets/images/preview-images/" + `${index}` + ".png";
       document.getElementById("preview-image").classList.remove("hide");
-
     });
     element.addEventListener("mouseout", function () {
       document.getElementById("preview-image").classList.add("hide");
     });
   });
+
+/* Carousel */
+const items = document.querySelector(".items");
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const total = items.children.length - 1;
+const active = "active";
+let current = 0;
+
+const scrollToCurrent = () => {
+  items.style.transform = `translateX(${current * -100}%`;
+};
+
+const scrollPrev = () => {
+  if (current === 0) current = total + 1;
+  current--;
+  scrollToCurrent();
+};
+
+const scrollNext = () => {
+  if (current === total) current = -1;
+  current++;
+  scrollToCurrent();
+};
+
+prev.addEventListener("click", scrollPrev);
+next.addEventListener("click", scrollNext);
