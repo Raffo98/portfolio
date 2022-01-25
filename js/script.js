b395b8802023a753bf10e2c52621664948562eb8
Raffo98 <raf.riccardelli@gmail.com>
2022-01-25 12:39:27 +0100

    update single pages

--- js/script.js
+++ js/script.js
@@ -17,12 +17,18 @@ document
 const items = document.querySelector(".items");
 const prev = document.querySelector(".prev");
 const next = document.querySelector(".next");
+const count = document.querySelector(".counter-img p");
 const total = items.children.length - 1;
 const active = "active";
 let current = 0;
 
+count.innerText = (current + 1) + "/" + items.children.length;
+
+console.log(count);
+
 const scrollToCurrent = () => {
   items.style.transform = `translateX(${current * -100}%`;
+  count.innerText = (current + 1) + "/" + items.children.length;
 };
 
 const scrollPrev = () => {
@@ -39,5 +45,3 @@ const scrollNext = () => {
 
 prev.addEventListener("click", scrollPrev);
 next.addEventListener("click", scrollNext);
-
-console.log("AOOAOAOAOOAOAO");
